a0397d59108915ddaafb6f412ec0bdd132d9f9c1
George Wadia <ggammil112@gmail.com>
2026-05-11 04:56:55 +0300

    Add Smart Followup System

--- app/scheduler/scheduler.py
+++ app/scheduler/scheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.services.content_factory import generate_daily_post
+from app.services.followup_service import process_followups
 
 scheduler = BackgroundScheduler()
 
@@ -9,4 +10,9 @@ def generate_content():
 
 def start_scheduler():
     scheduler.add_job(generate_content, 'interval', hours=24)
+    scheduler.add_job(
+    process_followups,
+    'interval',
+    hours=1
+    )
     scheduler.start()
