cc89b3ce404e615ed807c9b842163e4338f3e28a
George Wadia <ggammil112@gmail.com>
2026-05-13 22:52:01 +0300

    Add Facebook debug logs

--- app/services/facebook_service.py
+++ app/services/facebook_service.py
@@ -6,10 +6,8 @@ from app.config.settings import settings
 def send_message(recipient_id, text):
 
     url = (
-        "https://graph.facebook.com/"
-        "v21.0/me/messages"
-        f"?access_token="
-        f"{settings.FACEBOOK_PAGE_ACCESS_TOKEN}"
+        "https://graph.facebook.com/v21.0/me/messages"
+        f"?access_token={settings.FACEBOOK_PAGE_ACCESS_TOKEN}"
     )
 
     payload = {
@@ -26,25 +24,6 @@ def send_message(recipient_id, text):
         json=payload
     )
 
-    return response.json()
-
-
-def reply_to_comment(comment_id, message):
-
-    url = (
-        f"https://graph.facebook.com/"
-        f"v21.0/{comment_id}/comments"
-    )
-
-    payload = {
-        "message": message,
-        "access_token":
-        settings.FACEBOOK_PAGE_ACCESS_TOKEN
-    }
-
-    response = requests.post(
-        url,
-        data=payload
-    )
-
-    return response.json()
+    print("FACEBOOK RESPONSE:")
+    print(response.status_code)
+    print(response.text)
